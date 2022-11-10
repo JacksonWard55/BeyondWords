def9821e3627442a8531b5fc6f78dc5883564d28
JacksonWard55 <jacksonward@unomaha.edu>
2022-11-09 18:39:49 -0600

    finally

--- BeyondWords/src/app/home/home.module.ts
+++ BeyondWords/src/app/home/home.module.ts
@@ -6,13 +6,16 @@ import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    DragDropModule
   ],
   declarations: [HomePage]
 })
